d6420519ab8c19d3d1245b931e7137c2f93568a9
Lenovo 15 <information@com1software.com>
2024-12-06 14:57:06 -0500

    update

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+
 	"github.com/eiannone/keyboard"
 )
 
-func main() {		
+func main() {
+	reader := bufio.NewReader(os.Stdin)
 	if err := keyboard.Open(); err != nil {
 		panic(err)
 	}
@@ -13,15 +17,28 @@ func main() {
 		_ = keyboard.Close()
 	}()
 
-	fmt.Println("Press ESC to quit")
+	fmt.Println("DBU Database Utility")
+	fmt.Println("1 - View DBF File")
+	fmt.Println("   ESC - Exit")
+
 	for {
 		char, key, err := keyboard.GetKey()
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
-        if key == keyboard.KeyEsc {
+		if string(char) == "1" {
+			fmt.Println("Load file")
+
+			fmt.Print("Enter a line of text: ")
+			text, _ := reader.ReadString('\n') // Read until newline character
+			// Remove trailing newline
+			text = text[:len(text)-1]
+
+			fmt.Println("You entered:", text)
+		}
+		// fmt.Printf("You pressed: rune %q, key %X\r\n", char, key)
+		if key == keyboard.KeyEsc {
 			break
 		}
-	}	
+	}
 }
